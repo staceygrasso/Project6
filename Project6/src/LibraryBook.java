f99e44dec65d41064360a833f9603cc0beda8b89
staceygrasso <59070489+staceygrasso@users.noreply.github.com>
2020-03-03 15:43:52 -0500

    Add files via upload

---
+++ Project6/src/LibraryBook.java
@@ -0,0 +1,60 @@
+
+public class LibraryBook {
+
+    private String title;
+    private int copyrightYear;
+    private String isbnNum;
+    private String authorLastName;
+    private String authorFirstName;
+
+   //Constructors
+   //FINISH CODING THIS CONSTRUCTOR TO INITIALIZE ALL instance variables
+     public LibraryBook(String newtitle, String newisbn, String lname){
+
+    }
+
+
+   //provide set methods for all instance variable EXCEPT title and isbnNum HERE
+
+
+
+
+
+
+
+   //provide get methods for all instance variables HERE
+
+
+
+
+
+   //complete these three methods
+
+   public char authorLastInitial()  {
+
+
+   }
+
+   // this method prints out the authors name in form   LASTNAME, FIRSTNAMe
+   public void authorName() {
+
+
+   }
+
+   // this method prints out all info stored about this library book
+   public void displayAll() {
+
+
+   }
+
+   public static void main (String[] args) {
+
+
+
+
+
+
+    }
+
+}
+
